d9034e427a66a221ae2ef08e6fe63fb484254906
jsPC <wnstn9904@naver.com>
2024-04-22 20:03:22 +0900

    #02_ItemDetailPage_UI

--- lib/item_list_page.dart
+++ lib/item_list_page.dart
@@ -1,7 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_project/constants.dart';
+import 'package:flutter_practice_project/item_details.page.dart';
 import 'package:flutter_practice_project/models/product.dart';
-import 'package:intl/intl.dart';
 
 class ItemListPage extends StatefulWidget {
   const ItemListPage({super.key});
@@ -11,8 +12,6 @@ class ItemListPage extends StatefulWidget {
 }
 
 class _ItemListPageState extends State<ItemListPage> {
-  final NumberFormat numberFormat = NumberFormat('###,###,###,###');
-
   List<Product> productList = [
     Product(
         productNo: 1,
@@ -61,6 +60,7 @@ class _ItemListPageState extends State<ItemListPage> {
         ),
         itemBuilder: (context, index) {
           return productContainer(
+            productNo: productList[index].productNo ?? 0,
             productName: productList[index].productName ?? "",
             productImageUrl: productList[index].productImageUrl ?? "",
             price: productList[index].price ?? 0,
@@ -71,47 +71,64 @@ class _ItemListPageState extends State<ItemListPage> {
   }
 
   Widget productContainer({
+    required int productNo,
     required String productName,
     required String productImageUrl,
     required double price,
   }) {
-    return Container(
-      padding: const EdgeInsets.all(5),
-      child: Column(
-        children: [
-          CachedNetworkImage(
-            height: 150,
-            fit: BoxFit.cover,
-            imageUrl: productImageUrl,
-            placeholder: (context, url) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  strokeWidth: 2,
-                ),
-              );
-            },
-            errorWidget: (context, url, error) {
-              return const Center(
-                child: Text('오류 발생'),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return ItemDetailsPage(
+                productNo: productNo,
+                productName: productName,
+                productImageUrl: productImageUrl,
+                price: price,
               );
             },
           ),
-          Container(
-            padding: const EdgeInsets.all(3),
-            child: Text(
-              productName,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              height: 150,
+              fit: BoxFit.cover,
+              imageUrl: productImageUrl,
+              placeholder: (context, url) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2,
+                  ),
+                );
+              },
+              errorWidget: (context, url, error) {
+                return const Center(
+                  child: Text('오류 발생'),
+                );
+              },
+            ),
+            Container(
+              padding: const EdgeInsets.all(3),
+              child: Text(
+                productName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              "${numberFormat.format(price)}원",
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                "${numberFormat.format(price)}원",
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
