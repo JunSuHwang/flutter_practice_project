be5f619263bc49dbc6c1908b3be417312142babe
jsPC <wnstn9904@naver.com>
2024-05-03 19:20:18 +0900

    #09_AddToCart_Logic

--- lib/item_details.page.dart
+++ lib/item_details.page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_project/constants.dart';
@@ -22,6 +24,8 @@ class ItemDetailsPage extends StatefulWidget {
 }
 
 class _ItemDetailsPageState extends State<ItemDetailsPage> {
+  int quantity = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,48 +36,11 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(15),
-              child: CachedNetworkImage(
-                width: MediaQuery.of(context).size.width * 0.8,
-                imageUrl: widget.productImageUrl,
-                fit: BoxFit.cover,
-                placeholder: (context, url) {
-                  return const Center(
-                    child: CircularProgressIndicator(
-                      strokeWidth: 2,
-                    ),
-                  );
-                },
-                errorWidget: (context, url, error) {
-                  return const Center(
-                    child: Text('오류 발생'),
-                  );
-                },
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20,
-              ),
-              child: Text(
-                widget.productName,
-                textScaler: const TextScaler.linear(1.5),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20,
-              ),
-              child: Text(
-                "${numberFormat.format(widget.price)}원",
-                textScaler: const TextScaler.linear(1.3),
-              ),
-            ),
+            productImageContainer(),
+            productNameContainer(),
+            productPriceContainer(),
+            productQuantityContainer(),
+            productTotalPriceContainer(),
           ],
         ),
       ),
@@ -81,7 +48,22 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
         padding: const EdgeInsets.all(20),
         child: FilledButton(
           onPressed: () {
-            //! 추후 장바구니 담는 로직 추가 예정
+            //! 임시 장바구니 변수 Map 선언 - 디스크에서 받아옴
+            Map<String, dynamic> cartMap =
+                json.decode(sharedPreferences.getString("cartMap") ?? "{}") ??
+                    {};
+
+            //! 장바구니에 해당 제품이 없으면
+            if (cartMap[widget.productNo.toString()] == null) {
+              cartMap.addAll({widget.productNo.toString(): quantity});
+            } else {
+              //! 제품이 있으면
+              cartMap[widget.productNo.toString()] += quantity;
+            }
+            //! 디스크에 다시 반영
+            sharedPreferences.setString("cartMap", json.encode(cartMap));
+
+            print(cartMap);
 
             //! 장바구니 페이지로 이동
             Navigator.of(context).push(MaterialPageRoute(builder: (context) {
@@ -90,6 +72,116 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
           },
           child: const Text("장바구니 담기"),
         ),
+      ),
+    );
+  }
+
+  Widget productImageContainer() {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(15),
+      child: CachedNetworkImage(
+        width: MediaQuery.of(context).size.width * 0.8,
+        imageUrl: widget.productImageUrl,
+        fit: BoxFit.cover,
+        placeholder: (context, url) {
+          return const Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          );
+        },
+        errorWidget: (context, url, error) {
+          return const Center(
+            child: Text('오류 발생'),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget productNameContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      child: Text(
+        widget.productName,
+        textScaler: const TextScaler.linear(1.5),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget productPriceContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      child: Text(
+        "${numberFormat.format(widget.price)}원",
+        textScaler: const TextScaler.linear(1.3),
+      ),
+    );
+  }
+
+  Widget productQuantityContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("수량"),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                if (quantity > 1) {
+                  quantity--;
+                }
+              });
+            },
+            icon: const Icon(Icons.remove, size: 24),
+          ),
+          Text("$quantity"),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                quantity++;
+              });
+            },
+            icon: const Icon(
+              Icons.add,
+              size: 24,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget productTotalPriceContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            "총 상품 금액",
+            textScaler: TextScaler.linear(1.3),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "${numberFormat.format(widget.price * quantity)}원",
+            textScaler: const TextScaler.linear(1.3),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
